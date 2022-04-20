a9de571f7db27a0a4f1c7bd9036646bcdc83a52c
Administrator <admin@liucl.cn>
2022-04-20 21:21:08 +0800

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import {createApp} from 'oitq'
-import * as example from '@/plugins/example'
-
-import * as exampleJs from '@/plugins/example-js'
+import * as database from '@oitq/plugin-database'
+import * as qa from '@oitq/plugin-qa'
+import * as schedule from '@oitq/plugin-schedule'
+import * as common from '@oitq/plugin-common'
+import * as callme from '@oitq/plugin-callme'
+import * as music from '@oitq/plugin-music'
 const app=createApp()
-app.addBot({
-    uin:1689919782,
-    type:'qrcode',
-    config:{
-        platform:5
-    }
-})
-app.plugin(example)
-    .plugin(exampleJs)
+app.plugin(callme)
+    .plugin(music)
+    .plugin(common,{operator:1659488338})
+    .plugin(qa)
+    .plugin(schedule)
+    .plugin(database,{
+        dialect:'mysql',
+        host:'148.70.201.93',
+        database:'oitq',
+        username:'root',
+        password:'l196023.',
+        logging:()=>{}
+    })
 app.start(8086)
