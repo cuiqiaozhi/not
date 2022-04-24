df75f6843d4786e53324cca0f447f9e114a360e9
凉菜 <1659488338@qq.com>
2022-04-24 17:14:57 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@ import * as common from '@oitq/plugin-common'
 import * as callme from '@oitq/plugin-callme'
 import * as music from '@oitq/plugin-music'
 const app=createApp()
+app.addBot({
+        uin:147258369,
+        type:'qrcode',
+        config:{
+                platform:5
+        },
+        oneBot:false
+});
 app.plugin(callme)
     .plugin(music)
     .plugin(common,{operator:1659488338})
